c4989f24bc4a2872e8d84ad2189f5aa3fed7dc71
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-07-20 21:05:20 -0700

    updating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,21 @@
 require 'faker'
 Brewery.destroy_all
 
-
-#these are the real number of breweries in these cities ;)
-
 18.times do 
     Brewery.create(
-        name: Faker::Beer.brand + ' Brewery',
+        name: "#{Faker::Beer.brand.split.first} #{Faker::Beer.hop} Brewery",
         brewery_type: Faker::Beer.style,
         street: Faker::Address.street_address,
         city:  'Portland',
         state: 'Maine',
         postal_code: Faker::Address.zip_code,
-        website: "www.#{Faker::Beer.brand.split.first}.com"
+        website: "www.#{Faker::Beer.name.split.first}.com"
     )
 end
 
 17.times do 
     Brewery.create(
-        name: Faker::Beer.brand + ' Brewery',
+        name: "#{Faker::Beer.brand.split.first} #{Faker::Beer.hop} Brewery",
         brewery_type: Faker::Beer.style,
         street: Faker::Address.street_address,
         city:  'Asheville',
@@ -30,24 +27,24 @@ end
 
 16.times do 
     Brewery.create(
-        name: Faker::Beer.brand + ' Brewery',
+        name: "#{Faker::Beer.brand.split.first} #{Faker::Beer.hop} Brewery",
         brewery_type: Faker::Beer.style,
         street: Faker::Address.street_address,
         city:  'Bend',
         state: 'Oregon',
         postal_code: Faker::Address.zip_code,
-        website: "www.#{Faker::Beer.brand.split.first}.com"
+        website: "www.#{Faker::Beer.name.split.first}.com"
     )
 end
 
 14.times do 
     Brewery.create(
-        name: Faker::Beer.brand + ' Brewery',
+    name: "#{Faker::Beer.brand.split.first} #{Faker::Beer.hop} Brewery",
         brewery_type: Faker::Beer.style,
         street: Faker::Address.street_address,
         city:  'Boulder',
         state: 'Colorado',
         postal_code: Faker::Address.zip_code,
-        website: "www.#{Faker::Beer.brand.split.first}.com"
+        website: "www.#{Faker::Beer.name.split.first}.com"
     )
 end
