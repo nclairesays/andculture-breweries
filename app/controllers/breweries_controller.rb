3b1c079b0c1504973317cbe0c82c219930f40884
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-07-20 18:09:33 -0700

    added routes and logic to breweries controller

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,4 +1,15 @@
 class BreweriesController < ApplicationController
-
-    
+    def index
+        if(params[:by_city]) 
+            city = params[:by_city].capitalize
+            breweries = Brewery.all.select {|b| b.city === city}
+            # if unable to filter by_city, return all breweries
+            if(breweries.length === 0)
+                breweries = Brewery.all
+            end
+        else 
+            breweries = Brewery.all
+        end
+        render json: breweries
+    end
 end
